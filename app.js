9c21612b1328a49d89db130a1ed1f889826dd247
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-05-15 15:54:39 +0530

    Learned and implemented controllers in angular js

--- app.js
+++ app.js
@@ -2,14 +2,13 @@
 var myApp = angular.module("myapp", []);
 
 // Setting up the configurations that are required just before our application run
-myApp.config(function(){
-
-});
+// myApp.config(function(){});
 
 // Function to set-up when our application runs
-myApp.run(function(){
-
-});
+// myApp.run(function(){});
 
 // Declaring the controllers
-myApp.controller()
+myApp.controller("MyController", ["$scope", function($scope){
+    $scope.message = "This is my first implementation of the controller";
+    $scope.ninjas = ["Naruto", "Kakashi", "Itachi", "Sasuke", "Might Guy", "Obito"];
+}]);
