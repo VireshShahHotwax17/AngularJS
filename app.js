7792c84dec0ae0acf8c53cbe1525165325c90691
VireshShahHotwax17 <viresh.shah@hotwaxsystems.com>
2025-05-15 15:44:48 +0530

    Learned and implemented about the modules in angular js

---
+++ app.js
@@ -0,0 +1,15 @@
+// Creating a module
+var myApp = angular.module("myapp", []);
+
+// Setting up the configurations that are required just before our application run
+myApp.config(function(){
+
+});
+
+// Function to set-up when our application runs
+myApp.run(function(){
+
+});
+
+// Declaring the controllers
+myApp.controller()
